353cd2802fc1f54f99fd87848bd6de4183aa44bd
Sol Ise <solisenet@gmail.com>
2022-05-24 10:27:55 -0300

    Added Activity Service, Activity Controller with validation

---
+++ src/main/java/com/alkemy/ong/ports/input/rs/request/CreateActivityRequest.java
@@ -0,0 +1,33 @@
+package com.alkemy.ong.ports.input.rs.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateActivityRequest {
+
+    @NotBlank
+    @JsonProperty("name")
+    private String name;
+
+    @NotBlank
+    @JsonProperty("content")
+    private String content;
+
+    @JsonProperty("image")
+    private String image;
+}
