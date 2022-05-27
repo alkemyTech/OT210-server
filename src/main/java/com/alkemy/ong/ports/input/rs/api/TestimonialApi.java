037b9a8e9c01f405722964cd01eab7c82367f684
Cristian Orlando Michel <53406773+mike47k@users.noreply.github.com>
2022-05-27 09:37:56 -0300

    [OT210-65] Endpoint de testimonial creado (#11)

---
+++ src/main/java/com/alkemy/ong/ports/input/rs/api/TestimonialApi.java
@@ -0,0 +1,13 @@
+package com.alkemy.ong.ports.input.rs.api;
+
+import com.alkemy.ong.ports.input.rs.request.CreateTestimonialRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.Valid;
+
+@Validated
+public interface TestimonialApi {
+
+    ResponseEntity<Void> createTestimonial(@Valid CreateTestimonialRequest createTestimonialRequest);
+}
