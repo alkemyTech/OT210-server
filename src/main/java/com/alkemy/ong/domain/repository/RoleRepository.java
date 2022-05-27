c2fc39f9a0ac62d534e89296834b819834d47cd5
Hernán Federico Costarelli <92120975+hernancostarelli@users.noreply.github.com>
2022-05-27 02:29:31 -0300

    [OT210-17] Crear modelo y repositorio base de Usuarios
    Correcciones solicitadas hechas

--- src/main/java/com/alkemy/ong/domain/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alkemy.ong.domain.repository;
-
-import com.alkemy.ong.domain.model.Role;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface RoleRepository extends PagingAndSortingRepository<Role, Long> {
-}
