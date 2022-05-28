6798997ffc48c3655bfca62f2686da6c9444835c
Jonathan Manera <manerajona@gmail.com>
2022-05-28 08:59:15 -0300

    refactor

--- src/main/java/com/alkemy/ong/configuration/SendGridConfig.java
+++ src/main/java/com/alkemy/ong/configuration/SendGridConfig.java
@@ -4,17 +4,15 @@ import com.sendgrid.SendGrid;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:application.properties")
 public class SendGridConfig {
 
     @Value("${email.sendgrid.apikey}")
     String sendGridAPIKey;
 
     @Bean
-    public SendGrid getSendGridAPIKey(){
+    SendGrid getSendGridAPIKey(){
         return new SendGrid(sendGridAPIKey);
     }
 
--- src/main/java/com/alkemy/ong/ports/output/email/SendGridEmailService.java
+++ src/main/java/com/alkemy/ong/ports/output/email/SendGridEmailService.java
@@ -2,52 +2,49 @@ package com.alkemy.ong.ports.output.email;
 
 import com.sendgrid.Method;
 import com.sendgrid.Request;
-import com.sendgrid.Response;
 import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class SendGridEmailService implements EmailService {
 
-    private SendGrid sendGridClient;
+    private static final String NO_REPLY_SOMOSMAS_ORG = "no-reply@somosmas.org";
+    private final SendGrid sendGridClient;
+    @Value("${email.from}")
+    private String emailFrom;
 
-    @Autowired
-    public SendGridEmailService(SendGrid sendGridClient) {
-        this.sendGridClient = sendGridClient;
+    @Override
+    public void sendText(String to, String subject, String body) {
+        sendEmail(this.emailFrom, to, subject, new Content("text/plain", body));
     }
 
     @Override
-    public void sendText(String from, String to, String subject, String body) {
-        Response response = sendEmail(from, to, subject, new Content("text/plain", body));
+    public void sendHTML(String to, String subject, String body) {
+        sendEmail(this.emailFrom, to, subject, new Content("text/html", body));
     }
 
-    private Response sendEmail(String from, String to, String subject, Content content) {
+    private void sendEmail(String from, String to, String subject, Content content) {
         Mail mail = new Mail(new Email(from), subject, new Email(to), content);
-        mail.setReplyTo(new Email(System.getenv("SG_EMAILFROM")));
-        Request request = new Request();
-        Response response = new Response();
+        mail.setReplyTo(new Email(NO_REPLY_SOMOSMAS_ORG));
         try {
+            Request request = new Request();
             request.setMethod(Method.POST);
             request.setEndpoint("/mail/send");
             request.setBody(mail.build());
             this.sendGridClient.api(request);
+        } catch (IOException ex) {
+            log.error("Error sending email", ex);
+            throw new RuntimeException(ex);
         }
-        catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-        return response;
-    }
-
-    @Override
-    public void sendHTML(String from, String to, String subject, String body) {
-        Response response = sendEmail(from, to, subject, new Content("text/html", body));
-        System.out.println("Status Code: " + response.getStatusCode() +
-                ", Body: " + response.getBody() + ", Headers: " + response.getHeaders());
     }
 }
