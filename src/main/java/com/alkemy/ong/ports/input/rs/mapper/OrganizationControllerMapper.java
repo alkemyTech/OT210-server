58aafd857fda50e042d56dc00d0f5ff48f4f38d8
nicouema <nicouemacapdevila@gmail.com>
2022-05-28 21:18:52 -0300

    Update OrganizationControllerMapper.java

--- src/main/java/com/alkemy/ong/ports/input/rs/mapper/OrganizationControllerMapper.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/OrganizationControllerMapper.java
@@ -2,10 +2,7 @@ package com.alkemy.ong.ports.input.rs.mapper;
 
 import com.alkemy.ong.domain.model.Organization;
 import com.alkemy.ong.ports.input.rs.response.OrganizationResponse;
-import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 
 import java.util.List;
 
