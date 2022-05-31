6954c329a7ae4ee017eb52c30097d57c305b0551
Hernán Federico Costarelli <92120975+hernancostarelli@users.noreply.github.com>
2022-05-31 17:36:25 -0300

    [OT210-72] Crear Seed Data de Usuarios

--- src/main/resources/db/migration/V1.17__create_table_user.sql
+++ src/main/resources/db/migration/V1.17__create_table_user.sql
@@ -3,10 +3,10 @@ drop table if exists user;
 create table user
 (
     user_id bigint not null auto_increment,
-    first_name varchar(50) not null,
-    last_name varchar(50) not null,
-    email varchar(50) not null unique,
-    password varchar(255) not null,
+    first_name varchar(25) not null,
+    last_name varchar(25) not null,
+    email varchar(25) not null unique,
+    password varchar(25) not null,
     photo varchar(255) null,
     role_id bigint not null,
     is_active bit not null,
