aa244c0f682a05a0f10f3cc1dab9b2dfd1529747
vongolaARZ <alderetefacugg@gmail.com>
2022-05-31 12:58:19 -0300

    user authenticated detail endpoint

--- src/main/java/com/alkemy/ong/configuration/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/configuration/SecurityConfig.java
@@ -1,20 +1,55 @@
 package com.alkemy.ong.configuration;
 
+import com.alkemy.ong.common.exception.handler.AuthenticationEntryPointHandler;
+import com.alkemy.ong.common.exception.handler.CustomAccessDeniedHandler;
+import com.alkemy.ong.common.security.JwtRequestFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+    private final UserDetailsService userDetailsService;
+    private final JwtRequestFilter jwtRequestFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/auth").permitAll()
+                .anyRequest().hasRole("USER")
+                .and()
+                .exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPointHandler())
+                .accessDeniedHandler( new CustomAccessDeniedHandler())
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(STATELESS)
+                .and()
+                .addFilterBefore(jwtRequestFilter,UsernamePasswordAuthenticationFilter.class);
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean()throws Exception{
+        return super.authenticationManagerBean();
+    }
 }
