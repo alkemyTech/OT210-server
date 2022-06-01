e86be1f9ee7b57caa1958060653a977a18451a75
Cristian Michel <cristianmichel33@gmail.com>
2022-06-01 18:42:27 -0300

    resolviendo conflictos

--- src/main/java/com/alkemy/ong/ports/input/rs/mapper/UserControllerMapper.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/UserControllerMapper.java
@@ -4,6 +4,7 @@ import com.alkemy.ong.domain.model.User;
 import com.alkemy.ong.ports.input.rs.response.UserResponse;
 import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
 import java.util.List;
@@ -15,5 +16,10 @@ public interface UserControllerMapper extends CommonMapper{
     List<UserResponse> userListToUserListResponse(List<User> users);
 
     @Named("userToUserResponse")
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "firstName", source = "firstName")
+    @Mapping(target = "lastName", source ="lastName")
+    @Mapping(target = "email", source ="email")
+    @Mapping(target = "photo" , source = "photo")
     UserResponse userToUserResponse(User user);
 }
