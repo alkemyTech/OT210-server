160b026f844a3bc5e30bd165050784b57c24517d
Cristian Orlando Michel <53406773+mike47k@users.noreply.github.com>
2022-06-01 19:40:25 -0300

    [OT210-35] Endpoint para listar Usuarios (#30)

--- src/main/java/com/alkemy/ong/ports/input/rs/mapper/UserControllerMapper.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/UserControllerMapper.java
@@ -2,12 +2,19 @@ package com.alkemy.ong.ports.input.rs.mapper;
 
 import com.alkemy.ong.domain.model.User;
 import com.alkemy.ong.ports.input.rs.response.UserResponse;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
+import java.util.List;
+
 @Mapper
 public interface UserControllerMapper extends CommonMapper{
+
+    @IterableMapping(qualifiedByName = "userToUserResponse")
+    List<UserResponse> userListToUserListResponse(List<User> users);
+
 
     @Named("userToUserResponse")
     @Mapping(target = "id", source = "id")
