9cd6e6a5f9de776dc3980e2fdb5ab89f65a81445
Martin <101903286+vongolaARZ@users.noreply.github.com>
2022-06-01 15:25:14 -0300

    [OT210-36] Create UserDetails Endpoint (#26)

---
+++ src/main/java/com/alkemy/ong/ports/input/rs/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.alkemy.ong.ports.input.rs.controller;
+
+import com.alkemy.ong.domain.model.User;
+import com.alkemy.ong.ports.input.rs.mapper.UserControllerMapper;
+import com.alkemy.ong.ports.input.rs.response.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserControllerMapper userMapper;
+
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponse> getUserInformation(@AuthenticationPrincipal User user) {
+
+        UserResponse userResponse = userMapper.userToUserResponse(user);
+
+        return  new ResponseEntity<>(userResponse, HttpStatus.OK);
+
+    }
+}
---
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/UserControllerMapper.java
@@ -0,0 +1,19 @@
+package com.alkemy.ong.ports.input.rs.mapper;
+
+import com.alkemy.ong.domain.model.User;
+import com.alkemy.ong.ports.input.rs.response.UserResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+@Mapper
+public interface UserControllerMapper extends CommonMapper{
+
+    @Named("userToUserResponse")
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "firstName", source = "firstName")
+    @Mapping(target = "lastName", source ="lastName")
+    @Mapping(target = "email", source ="email")
+    @Mapping(target = "photo" , source = "photo")
+    UserResponse userToUserResponse(User user);
+}
