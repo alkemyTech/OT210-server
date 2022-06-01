9ae52ea3bb2ef0c7e95157756d153e5e45dfb6cc
nicouema <nicouemacapdevila@gmail.com>
2022-06-01 14:31:04 -0300

    Update OrganizationService.java

--- src/main/java/com/alkemy/ong/domain/usecase/OrganizationService.java
+++ src/main/java/com/alkemy/ong/domain/usecase/OrganizationService.java
@@ -1,7 +1,6 @@
 package com.alkemy.ong.domain.usecase;
 
 import com.alkemy.ong.domain.model.Organization;
-import org.springframework.data.domain.PageRequest;
 
 public interface OrganizationService {
 
