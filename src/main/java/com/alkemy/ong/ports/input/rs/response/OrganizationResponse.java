253e92896892b8ba3fb9826ec74ea3c6cb583e25
nicouema <nicouemacapdevila@gmail.com>
2022-06-02 18:13:57 -0300

    Update OrganizationResponse.java

--- src/main/java/com/alkemy/ong/ports/input/rs/response/OrganizationResponse.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/response/OrganizationResponse.java
@@ -1,6 +1,5 @@
 package com.alkemy.ong.ports.input.rs.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
