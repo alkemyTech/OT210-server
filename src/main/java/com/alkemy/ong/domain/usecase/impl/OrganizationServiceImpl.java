ac6a1d09ed89a32dae3c5a585575b997108b51ac
Nico Uema <101826700+nicouema@users.noreply.github.com>
2022-06-02 14:06:32 -0300

    [OT210-37] Organization - Put Method  (#32)
    * [OT210-37]
    
    Put Method - Organization
    
    * Update OrganizationController.java
    
    * Update OrganizationService.java
    
    * Update OrganizationServiceImpl.java
    
    Correction
    
    * Update OrganizationController.java
    
    Add 204 NO CONTENT response
    
    * Update OrganizationServiceImpl.java

--- src/main/java/com/alkemy/ong/domain/usecase/impl/OrganizationServiceImpl.java
+++ src/main/java/com/alkemy/ong/domain/usecase/impl/OrganizationServiceImpl.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 @Service
 @RequiredArgsConstructor
 public class OrganizationServiceImpl implements OrganizationService {
@@ -19,5 +20,20 @@ public class OrganizationServiceImpl implements OrganizationService {
     @Transactional(readOnly = true)
     public Organization getByIdIfExists(Long id) {
         return organizationRepository.findById(id).orElseThrow(() -> new NotFoundException(id));
+    }
+
+    @Override
+    @Transactional
+    public void updateEntityIfExists(Long id, Organization organization) {
+        organizationRepository.findById(id)
+                .map(organizationJpa -> {
+                    organizationJpa.setName(organization.getName());
+                    organizationJpa.setImage(organization.getImage());
+                    organizationJpa.setAddress(organization.getAddress());
+                    organizationJpa.setPhone(organization.getPhone());
+                    organizationJpa.setWelcomeText(organization.getWelcomeText());
+                    organizationJpa.setAboutUsText(organization.getAboutUsText());
+                    return organizationRepository.save(organizationJpa);
+                }).orElseThrow(() -> new NotFoundException(id));
     }
 }
--- src/main/java/com/alkemy/ong/ports/input/rs/mapper/OrganizationControllerMapper.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/OrganizationControllerMapper.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.ports.input.rs.mapper;
 
 import com.alkemy.ong.domain.model.Organization;
+import com.alkemy.ong.ports.input.rs.request.UpdateOrganizationRequest;
 import com.alkemy.ong.ports.input.rs.response.OrganizationResponse;
 import org.mapstruct.Mapper;
 
@@ -8,4 +9,6 @@ import org.mapstruct.Mapper;
 public interface OrganizationControllerMapper extends CommonMapper{
 
     OrganizationResponse organizationToOrganizationResponse(Organization organization);
+
+    Organization updateOrganizationRequestToOrganization(UpdateOrganizationRequest updateOrganizationRequest);
 }
