ee18f62c1c1ba24d4e795924b48dd7ca9c40ff38
nicouema <nicouemacapdevila@gmail.com>
2022-06-02 17:13:12 -0300

    [OT210-63]
    Contacts added to Organization attributes

--- src/main/java/com/alkemy/ong/ports/input/rs/response/OrganizationResponse.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/response/OrganizationResponse.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.ports.input.rs.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +11,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrganizationResponse {
+
     private String name;
+
     private String image;
+
     private Integer phone;
+
     private String address;
+
+    @JsonProperty("facebook_contact")
+    private String facebookContact;
+
+    @JsonProperty("linkedin_contact")
+    private String linkedinContact;
+
+    @JsonProperty("instagram_contact")
+    private String instagramContact;
 }
