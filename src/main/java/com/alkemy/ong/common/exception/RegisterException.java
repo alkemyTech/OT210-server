96d0d6578585c421d6a9395f4e5ed78f864dcf71
Hernán Federico Costarelli <92120975+hernancostarelli@users.noreply.github.com>
2022-06-03 17:05:40 -0300

    QT210-31] Añadir lógica de JWT

--- src/main/java/com/alkemy/ong/common/exception/RegisterException.java
+++
@@ -1,8 +0,0 @@
-package com.alkemy.ong.common.exception;
-
-public class RegisterException extends RuntimeException {
-
-    public RegisterException(String errorMsg) {
-        super(errorMsg);
-    }
-}
