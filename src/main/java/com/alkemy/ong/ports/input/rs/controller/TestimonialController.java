9687b9d220da5cb73f57dc8fc835e9013917e620
vongolaARZ <alderetefacugg@gmail.com>
2022-06-07 15:34:15 -0300

    corrections

--- src/main/java/com/alkemy/ong/ports/input/rs/controller/TestimonialController.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/controller/TestimonialController.java
@@ -8,11 +8,7 @@ import com.alkemy.ong.ports.input.rs.request.CreateTestimonialRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
@@ -31,6 +27,7 @@ public class TestimonialController implements TestimonialApi {
     private final TestimonialService service;
 
     @Override
+    @PostMapping
     public ResponseEntity<Void> createTestimonial(@Valid @RequestBody CreateTestimonialRequest createTestimonialRequest) {
 
         Testimonial testimonial = mapper.createTestimonialRequestToTestimonial(createTestimonialRequest);
