38dd54560f5e9b49cba0c3dd55180b4e239f60fd
vongolaARZ <alderetefacugg@gmail.com>
2022-06-08 12:37:53 -0300

    Slide GET endpoint implement

---
+++ src/main/java/com/alkemy/ong/ports/input/rs/response/SlideResponse.java
@@ -0,0 +1,21 @@
+package com.alkemy.ong.ports.input.rs.response;
+
+import com.alkemy.ong.domain.model.Organization;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SlideResponse {
+
+     private String image;
+     private String text;
+     private Integer order;
+     private Organization organization;
+
+
+}
