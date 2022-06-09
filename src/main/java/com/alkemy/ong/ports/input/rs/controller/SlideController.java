238fe27daa44e9c2ce26401fb25e053de96b3dd8
vongolaARZ <alderetefacugg@gmail.com>
2022-06-08 22:37:27 -0300

    properties modification

--- src/main/java/com/alkemy/ong/ports/input/rs/controller/SlideController.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/controller/SlideController.java
@@ -12,9 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.validation.constraints.NotNull;
-
 import static com.alkemy.ong.ports.input.rs.api.ApiConstants.SLIDES_URI;
 
 
