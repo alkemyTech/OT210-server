91191b5bb7ccc47778584f05b34915b798319792
vongolaARZ <alderetefacugg@gmail.com>
2022-06-08 22:24:56 -0300

    resolve conflict

--- src/main/java/com/alkemy/ong/domain/model/Category.java
+++ src/main/java/com/alkemy/ong/domain/model/Category.java
@@ -12,22 +12,15 @@ import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 import javax.persistence.*;
 import java.util.Objects;
-<<<<<<< HEAD
-=======
 import java.util.Set;
 
->>>>>>> a7a62cd9c03c981b06157be31b9483b995e64047
 
 @Getter
 @Setter
 @NoArgsConstructor
 @ToString
 @Where(clause = "is_active=true")
-<<<<<<< HEAD
-@SQLDelete(sql = "UPDATE categories SET is_active=false WHERE category_id=?")
-=======
 @SQLDelete(sql = "UPDATE category SET is_active=false WHERE category_id=?")
->>>>>>> a7a62cd9c03c981b06157be31b9483b995e64047
 @Entity
 @Table(name = "category")
 @EntityListeners(AuditListener.class)
@@ -50,18 +43,15 @@ public class Category implements Auditable {
     @Embedded
     private Audit audit;
 
-<<<<<<< HEAD
-=======
     @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
     @ToString.Exclude
     private Set<New> news;
 
->>>>>>> a7a62cd9c03c981b06157be31b9483b995e64047
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-       Category category = (Category) o;
+        Category category = (Category) o;
         return Objects.equals(id, category.id);
     }
 
