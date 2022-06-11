a8e57149054e51419de62326ced600a528404934
Hernán Federico Costarelli <92120975+hernancostarelli@users.noreply.github.com>
2022-06-11 17:15:35 -0300

    [OT210-82] Add Pagination to Members created (#63)

--- src/main/java/com/alkemy/ong/ports/input/rs/controller/MemberController.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/controller/MemberController.java
@@ -1,14 +1,20 @@
 package com.alkemy.ong.ports.input.rs.controller;
 
 import com.alkemy.ong.domain.model.Member;
+import com.alkemy.ong.domain.model.MemberList;
 import com.alkemy.ong.domain.usecase.MemberService;
+import com.alkemy.ong.ports.input.rs.api.ApiConstants;
 import com.alkemy.ong.ports.input.rs.api.MemberApi;
 import com.alkemy.ong.ports.input.rs.mapper.MemberControllerMapper;
 import com.alkemy.ong.ports.input.rs.request.CreateMemberRequest;
+import com.alkemy.ong.ports.input.rs.response.MemberResponse;
+import com.alkemy.ong.ports.input.rs.response.MemberResponseList;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +25,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 import static com.alkemy.ong.ports.input.rs.api.ApiConstants.MEMBERS_URI;
 
@@ -47,9 +55,37 @@ public class MemberController implements MemberApi {
     }
 
     @Override
+    @GetMapping
+    public ResponseEntity<MemberResponseList> getMembers(Optional<Integer> page, Optional<Integer> size) {
+
+        final int pageNumber = page.filter(p -> p > 0).orElse(ApiConstants.DEFAULT_PAGE);
+        final int pageSize = size.filter(s -> s > 0).orElse(ApiConstants.DEFAULT_PAGE_SIZE);
+
+        MemberList memberList = service.getList(PageRequest.of(pageNumber, pageSize));
+
+        MemberResponseList responseList;
+        {
+            responseList = new MemberResponseList();
+
+            List<MemberResponse> memberResponseList = mapper.memberListToMemberResponseList(memberList.getContent());
+            responseList.setContent(memberResponseList);
+
+            final int nextPage = memberList.getPageable().next().getPageNumber();
+            responseList.setNextUri(ApiConstants.uriByPageAsString.apply(nextPage));
+
+            final int previousPage = memberList.getPageable().previousOrFirst().getPageNumber();
+            responseList.setPreviousUri(ApiConstants.uriByPageAsString.apply(previousPage));
+
+            responseList.setTotalPages(memberList.getTotalPages());
+            responseList.setTotalElements(memberList.getTotalElements());
+        }
+        return ResponseEntity.ok().body(responseList);
+    }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteMember(@NotNull @PathVariable Long id) {
         service.deleteMember(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
+
