8290c0d37c22a669c67b8ca53847d181c9cbd656
Jonathan Manera <manerajona@gmail.com>
2022-06-11 17:03:45 -0300

    remove auth

--- src/main/java/com/alkemy/ong/ports/input/rs/api/ContactApi.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/api/ContactApi.java
@@ -1,13 +1,11 @@
 package com.alkemy.ong.ports.input.rs.api;
 
 import com.alkemy.ong.ports.input.rs.request.CreateContactRequest;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
 
-@SecurityRequirement(name = "bearerAuth")
 @Validated
 public interface ContactApi {
 
