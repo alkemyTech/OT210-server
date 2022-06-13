1c265556e3017b800f715a35b609e51282faf805
Martin <101903286+vongolaARZ@users.noreply.github.com>
2022-06-13 19:27:25 -0300

    [OT210-56]  Get Slide Endpoint (#57)

---
+++ src/main/java/com/alkemy/ong/domain/usecase/impl/SlideServiceImpl.java
@@ -0,0 +1,26 @@
+package com.alkemy.ong.domain.usecase.impl;
+
+import com.alkemy.ong.common.exception.NotFoundException;
+import com.alkemy.ong.domain.model.Slide;
+import com.alkemy.ong.domain.repository.SlideRepository;
+import com.alkemy.ong.domain.usecase.SlideService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@RequiredArgsConstructor
+public class SlideServiceImpl implements SlideService {
+
+    private final SlideRepository slideRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public Slide getByIdIfExist(Long id) {
+        return slideRepository.findById(id)
+                .orElseThrow(() ->new NotFoundException(id));
+    }
+
+}
