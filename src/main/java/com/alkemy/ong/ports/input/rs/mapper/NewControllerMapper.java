f1560af6591324db134061d0b3354dab0d54f2e3
Cristian Michel <cristianmichel33@gmail.com>
2022-06-13 22:51:16 -0300

    Agregar paginación a Novedades

--- src/main/java/com/alkemy/ong/ports/input/rs/mapper/NewControllerMapper.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/NewControllerMapper.java
@@ -2,10 +2,21 @@ package com.alkemy.ong.ports.input.rs.mapper;
 
 import com.alkemy.ong.domain.model.New;
 import com.alkemy.ong.ports.input.rs.request.CreateNewRequest;
+import com.alkemy.ong.ports.input.rs.response.NewResponse;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Named;
+
+import java.util.List;
 
 @Mapper
 public interface NewControllerMapper extends CommonMapper {
+
+    @IterableMapping(qualifiedByName = "newToNewResponse")
+    List<NewResponse> newListToNewResponseList(List<New> news);
+
+    @Named("newToNewResponse")
+    NewResponse newToNewResponse(New news);
 
     New createNewRequestToNew(CreateNewRequest create);
 }
