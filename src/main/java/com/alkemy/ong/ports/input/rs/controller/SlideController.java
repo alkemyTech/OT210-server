c700cbf0c548b2a850b25c9475d63a076eb3f728
vongolaARZ <alderetefacugg@gmail.com>
2022-06-15 09:05:05 -0300

    correction

--- src/main/java/com/alkemy/ong/ports/input/rs/controller/SlideController.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/controller/SlideController.java
@@ -1,6 +1,6 @@
 package com.alkemy.ong.ports.input.rs.controller;
 
-import com.alkemy.ong.domain.model.Slide;
+
 import com.alkemy.ong.domain.usecase.SlideService;
 import com.alkemy.ong.ports.input.rs.api.SlideApi;
 import com.alkemy.ong.ports.input.rs.request.SlideRequest;
@@ -12,10 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 
 import javax.validation.Valid;
-import java.net.URI;
+
 
 import static com.alkemy.ong.ports.input.rs.api.ApiConstants.SLIDES_URI;
 
