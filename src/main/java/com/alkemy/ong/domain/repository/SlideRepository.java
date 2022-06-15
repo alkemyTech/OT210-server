1f49d9c412699e68a359a870cc5d2eccad52d2b4
vongolaARZ <alderetefacugg@gmail.com>
2022-06-15 10:06:29 -0300

    corrections and solved conflicts

--- src/main/java/com/alkemy/ong/domain/repository/SlideRepository.java
+++ src/main/java/com/alkemy/ong/domain/repository/SlideRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface SlideRepository extends PagingAndSortingRepository<Slide, Long> {
 
-    @Query(nativeQuery = true, value = "SELECT max(order) FROM slide s")
+    @Query(nativeQuery = true, value = "SELECT max(slide_order) FROM slide ")
     Integer getMaxOrder();
 }
