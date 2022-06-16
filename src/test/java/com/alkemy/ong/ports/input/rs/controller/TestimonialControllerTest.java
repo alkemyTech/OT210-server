477328817faf0e91f1b42dd91eb292d409bc5e66
nicouema <nicouemacapdevila@gmail.com>
2022-06-16 13:33:38 -0300

    Update TestimonialControllerTest.java

--- src/test/java/com/alkemy/ong/ports/input/rs/controller/TestimonialControllerTest.java
+++ src/test/java/com/alkemy/ong/ports/input/rs/controller/TestimonialControllerTest.java
@@ -3,10 +3,13 @@ package com.alkemy.ong.ports.input.rs.controller;
 import com.alkemy.ong.common.exception.handler.GlobalExceptionHandler;
 import com.alkemy.ong.common.util.JsonUtils;
 import com.alkemy.ong.domain.model.Testimonial;
+import com.alkemy.ong.domain.model.TestimonialList;
 import com.alkemy.ong.domain.usecase.TestimonialService;
 import com.alkemy.ong.ports.input.rs.api.ApiConstants;
 import com.alkemy.ong.ports.input.rs.mapper.TestimonialControllerMapper;
 import com.alkemy.ong.ports.input.rs.request.CreateTestimonialRequest;
+import com.alkemy.ong.ports.input.rs.response.TestimonialResponse;
+import com.alkemy.ong.ports.input.rs.response.TestimonialResponseList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,17 +18,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.awt.*;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -72,14 +78,34 @@ public class TestimonialControllerTest {
     }
 
     @Test
-    void getTestimonial_shouldReturn200() throws Exception {
+    void getTestimonials_shouldReturn200() throws Exception {
 
         Testimonial testimonial = new Testimonial();
+        testimonial.setId(99L);
         testimonial.setName("TestimonialName");
         testimonial.setContent("TestimonialContent");
         testimonial.setImage("TestimonialImage");
-        testimonial.setId(99L);
 
-//        given(service.get)
+        TestimonialList list = new TestimonialList(List.of(testimonial), Pageable.ofSize(1), 1);
+
+        given(service.getList(any(PageRequest.class))).willReturn(list);
+
+        String content = mockMvc.perform(get(ApiConstants.TESTIMONIALS_URI + "?page=0&size=1"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        assertThat(content).isNotBlank();
+
+        TestimonialResponseList response = JsonUtils.jsonToObject(content, TestimonialResponseList.class);
+
+        assertThat(response.getTotalElements()).isEqualTo(1);
+        assertThat(response.getTotalPages()).isEqualTo(1);
+        assertThat(response.getNextUri()).isEqualTo("http://localhost/v1/testimonials?size=1&page=1");
+        assertThat(response.getPreviousUri()).isEqualTo("http://localhost/v1/testimonials?size=1&page=0");
+        assertThat(response.getContent()).isNotEmpty();
+
     }
 }
