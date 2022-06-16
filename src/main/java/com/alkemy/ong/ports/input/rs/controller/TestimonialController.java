7c29655fa5bdd356a3b7e27a75470d48b368e1a3
nicouema <nicouemacapdevila@gmail.com>
2022-06-16 08:55:44 -0300

    Update TestimonialController.java
    Correction

--- src/main/java/com/alkemy/ong/ports/input/rs/controller/TestimonialController.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/controller/TestimonialController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -63,7 +64,7 @@ public class TestimonialController implements TestimonialApi {
         Testimonial updated = service.updateIfExists(id, testimonial);
         TestimonialResponse testimonialResponse = mapper.testimonialToTestimonialResopnse(updated);
 
-        return new ResponseEntity<>(testimonialResponse, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @Override
