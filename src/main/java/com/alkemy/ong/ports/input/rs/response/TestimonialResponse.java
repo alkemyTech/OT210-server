04773ecc0e73f3b823f5735c23ff8940b821d885
Martin <101903286+vongolaARZ@users.noreply.github.com>
2022-06-17 19:04:24 -0300

    [OT210-57] Creation Slides Endpoint (#65)

--- src/main/java/com/alkemy/ong/ports/input/rs/response/TestimonialResponse.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/response/TestimonialResponse.java
@@ -1,13 +1,12 @@
 package com.alkemy.ong.ports.input.rs.response;
 
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
+
 
 
 @Data
@@ -15,7 +14,6 @@ import javax.validation.constraints.NotBlank;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TestimonialResponse {
-
 
     private String name;
     private String image;
--- src/main/java/com/alkemy/ong/ports/output/s3/S3Service.java
+++ src/main/java/com/alkemy/ong/ports/output/s3/S3Service.java
@@ -4,4 +4,5 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface S3Service {
     String uploadFile(MultipartFile file);
+    String uploadFile(String fileBase64, String fileName);
 }
