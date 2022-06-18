9fcb598725eea31fc2962289c910c1d551535df6
ArielF96 <98747908+ArielF96@users.noreply.github.com>
2022-06-18 11:47:17 -0300

    [OT210-55] Endpoint Listado de Slides (#73)

--- src/main/java/com/alkemy/ong/ports/input/rs/api/SlideApi.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/api/SlideApi.java
@@ -1,17 +1,39 @@
 package com.alkemy.ong.ports.input.rs.api;
 
-import com.alkemy.ong.ports.input.rs.request.SlideRequest;
+import com.alkemy.ong.common.exception.error.ErrorDetails;
+import com.alkemy.ong.ports.input.rs.response.ContactResponseList;
+import com.alkemy.ong.ports.input.rs.response.SlideResponseList;
+import com.alkemy.ong.ports.input.rs.response.SlideResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import java.util.Optional;
+import com.alkemy.ong.ports.input.rs.request.SlideRequest;
 import javax.validation.Valid;
-import com.alkemy.ong.ports.input.rs.response.SlideResponse;
 import javax.validation.constraints.NotNull;
 
-
+@SecurityRequirement(name = "bearerAuth")
 @Validated
 public interface SlideApi {
 
     ResponseEntity<Void> createSlide(@Valid SlideRequest slideRequest);
     ResponseEntity<SlideResponse> getById(@NotNull Long id);
     ResponseEntity<Void> deleteSlide(@NotNull Long id);
+
+    @Operation(summary = "Get Slide List", description = "Get Slide List", responses = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ContactResponseList.class))}),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
+                            examples = @ExampleObject(value = "{\"code\":\"BAD_CREDENTIALS\",\"detail\":\"The server cannot return a response due to invalid credentials.\"}"))}),
+    })
+    ResponseEntity<SlideResponseList> getSlides(Optional<Integer> page, Optional<Integer> size);
 }
--- src/main/java/com/alkemy/ong/ports/input/rs/mapper/SlideControllerMapper.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/SlideControllerMapper.java
@@ -2,15 +2,18 @@ package com.alkemy.ong.ports.input.rs.mapper;
 
 import com.alkemy.ong.domain.model.Slide;
 import com.alkemy.ong.ports.input.rs.response.SlideResponse;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
 
 @Mapper
-public interface SlideControllerMapper  extends CommonMapper{
+public interface SlideControllerMapper extends CommonMapper {
 
-    @Mapping(target = "image", source = "imageUrl")
-    @Mapping(target = "text", source = "text")
-    @Mapping(target = "order",source = "order")
-    @Mapping(target = "organization", source = "organization")
+    @IterableMapping(qualifiedByName = "slideToSlideResponse")
+    List<SlideResponse> slideListToSlideResponseList(List<Slide> slides);
+
+    @Named("slideToSlideResponse")
     SlideResponse slideToSlideResponse(Slide slide);
 }
