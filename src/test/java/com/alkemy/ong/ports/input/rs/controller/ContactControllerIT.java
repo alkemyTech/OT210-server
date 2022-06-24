c410c6aaae7e58aaa9875c796adfc6feb443e11c
vongolaARZ <alderetefacugg@gmail.com>
2022-06-23 23:30:26 -0300

    first commit

---
+++ src/test/java/com/alkemy/ong/ports/input/rs/controller/ContactControllerIT.java
@@ -0,0 +1,7 @@
+package com.alkemy.ong.ports.input.rs.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ContactControllerIT {
+}
---
+++ src/test/java/com/alkemy/ong/ports/input/rs/controller/ContactControllerTest.java
@@ -0,0 +1,16 @@
+package com.alkemy.ong.ports.input.rs.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactControllerTest {
+
+    @Test
+    void createContact() {
+    }
+
+    @Test
+    void getContacts() {
+    }
+}
